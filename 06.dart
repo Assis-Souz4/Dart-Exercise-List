86b3f57049e311a63819c4710cefff764980501c
Assis-Souz4 <assissouza-dev@hotmail.com>
2024-03-28 15:03:53 -0300

    exerc 6 ok

--- 06.dart
+++ 06.dart
@@ -1,8 +1,24 @@
+import 'dart:math';
+
+class Cilindro{
+  double volume = 0;
+  double raio;
+  double altura;
+
+  Cilindro(this.raio, this.altura);
+
+  double calcVolume(){
+    volume = pi * (raio * raio) * altura;
+    print('Volume do Cilindro é: ${volume.toStringAsFixed(2)}');
+    return volume;
+  }
+
+}
 void main(List<String> args) {
   //6) Escreva um programa que leia a altura e o raio de um cilindro circular e imprima o volume do mesmo, segundo a fórmula: V = pi.raio².altura ->obs: Imprima o volume com uma precisão de duas casas decimais.
 
-
-  
+  Cilindro cilindro1 = Cilindro(50, 100);
+  cilindro1.calcVolume();
 }
 
 
